a0dfd4d9afcf5269d567b9c9b13313aa56be83cc
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2025-05-30 15:39:25 +0700

    fix: bootstrap

--- scripts/bootstrap.sql
+++ scripts/bootstrap.sql
@@ -1,108 +1,136 @@
-CREATE TABLE db.nation ( N_NATIONKEY  INTEGER NOT NULL,
-                            N_NAME       CHAR(25) NOT NULL,
-                            N_REGIONKEY  INTEGER NOT NULL,
-                            N_COMMENT    VARCHAR(152))
-USING iceberg
-LOCATION '/mnt/project/warehouse/nation';
+-- Nation
+CREATE TABLE db.nation (
+  N_NATIONKEY  INTEGER NOT NULL,
+  N_NAME       STRING NOT NULL,
+  N_REGIONKEY  INTEGER NOT NULL,
+  N_COMMENT    STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/nation.csv'
-INTO TABLE db.nation;
+INSERT INTO db.nation
+SELECT CAST(_c0 AS INT), _c1, CAST(_c2 AS INT), _c3
+FROM csv.`/mnt/project/seeds/nation.csv`;
 
-CREATE TABLE db.region ( R_REGIONKEY  INTEGER NOT NULL,
-                            R_NAME       CHAR(25) NOT NULL,
-                            R_COMMENT    VARCHAR(152))
-USING iceberg
-LOCATION '/mnt/project/warehouse/region';
+-- Region
+CREATE TABLE db.region (
+  R_REGIONKEY  INTEGER NOT NULL,
+  R_NAME       STRING NOT NULL,
+  R_COMMENT    STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/region.csv'
-INTO TABLE db.region;
+INSERT INTO db.region
+SELECT CAST(_c0 AS INT), _c1, _c2
+FROM csv.`/mnt/project/seeds/region.csv`;
 
-CREATE TABLE db.part  ( P_PARTKEY     INTEGER NOT NULL,
-                          P_NAME        VARCHAR(55) NOT NULL,
-                          P_MFGR        CHAR(25) NOT NULL,
-                          P_BRAND       CHAR(10) NOT NULL,
-                          P_TYPE        VARCHAR(25) NOT NULL,
-                          P_SIZE        INTEGER NOT NULL,
-                          P_CONTAINER   CHAR(10) NOT NULL,
-                          P_RETAILPRICE DECIMAL(15,2) NOT NULL,
-                          P_COMMENT     VARCHAR(23) NOT NULL )
-USING iceberg
-LOCATION '/mnt/project/warehouse/part';
+-- Part
+CREATE TABLE db.part (
+  P_PARTKEY     INTEGER NOT NULL,
+  P_NAME        STRING NOT NULL,
+  P_MFGR        STRING NOT NULL,
+  P_BRAND       STRING NOT NULL,
+  P_TYPE        STRING NOT NULL,
+  P_SIZE        INTEGER NOT NULL,
+  P_CONTAINER   STRING NOT NULL,
+  P_RETAILPRICE DECIMAL(15,2) NOT NULL,
+  P_COMMENT     STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/part.csv'
-INTO TABLE db.part;
+INSERT INTO db.part
+SELECT CAST(_c0 AS INT), _c1, _c2, _c3, _c4, CAST(_c5 AS INT), _c6, CAST(_c7 AS DECIMAL(15,2)), _c8
+FROM csv.`/mnt/project/seeds/part.csv`;
 
-CREATE TABLE db.supplier ( S_SUPPKEY     INTEGER NOT NULL,
-                             S_NAME        CHAR(25) NOT NULL,
-                             S_ADDRESS     VARCHAR(40) NOT NULL,
-                             S_NATIONKEY   INTEGER NOT NULL,
-                             S_PHONE       CHAR(15) NOT NULL,
-                             S_ACCTBAL     DECIMAL(15,2) NOT NULL,
-                             S_COMMENT     VARCHAR(101) NOT NULL)
-USING iceberg
-LOCATION '/mnt/project/warehouse/supplier';
+-- Supplier
+CREATE TABLE db.supplier (
+  S_SUPPKEY     INTEGER NOT NULL,
+  S_NAME        STRING,
+  S_ADDRESS     STRING,
+  S_NATIONKEY   INTEGER,
+  S_PHONE       STRING,
+  S_ACCTBAL     DECIMAL(15,2),
+  S_COMMENT     STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/supplier.csv'
-INTO TABLE db.supplier;
+INSERT INTO db.supplier
+SELECT CAST(_c0 AS INT), _c1, _c2, CAST(_c3 AS INT), _c4, CAST(_c5 AS DECIMAL(15,2)), _c6
+FROM csv.`/mnt/project/seeds/supplier.csv`;
 
-CREATE TABLE db.partsupp ( PS_PARTKEY     INTEGER NOT NULL,
-                             PS_SUPPKEY     INTEGER NOT NULL,
-                             PS_AVAILQTY    INTEGER NOT NULL,
-                             PS_SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
-                             PS_COMMENT     VARCHAR(199) NOT NULL )
-USING iceberg
-LOCATION '/mnt/project/warehouse/partsupp';
+-- Partsupp
+CREATE TABLE db.partsupp (
+  PS_PARTKEY     INTEGER NOT NULL,
+  PS_SUPPKEY     INTEGER NOT NULL,
+  PS_AVAILQTY    INTEGER NOT NULL,
+  PS_SUPPLYCOST  DECIMAL(15,2) NOT NULL,
+  PS_COMMENT     STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/partsupp.csv'
-INTO TABLE db.partsupp;
+INSERT INTO db.partsupp
+SELECT CAST(_c0 AS INT), CAST(_c1 AS INT), CAST(_c2 AS INT), CAST(_c3 AS DECIMAL(15,2)), _c4
+FROM csv.`/mnt/project/seeds/partsupp.csv`;
 
-CREATE TABLE dbt.customer ( C_CUSTKEY     INTEGER NOT NULL,
-                             C_NAME        VARCHAR(25) NOT NULL,
-                             C_ADDRESS     VARCHAR(40) NOT NULL,
-                             C_NATIONKEY   INTEGER NOT NULL,
-                             C_PHONE       CHAR(15) NOT NULL,
-                             C_ACCTBAL     DECIMAL(15,2)   NOT NULL,
-                             C_MKTSEGMENT  CHAR(10) NOT NULL,
-                             C_COMMENT     VARCHAR(117) NOT NULL)
-USING iceberg
-LOCATION '/mnt/project/warehouse/customer';
+-- Customer
+CREATE TABLE db.customer (
+  C_CUSTKEY     INTEGER NOT NULL,
+  C_NAME        STRING,
+  C_ADDRESS     STRING,
+  C_NATIONKEY   INTEGER,
+  C_PHONE       STRING,
+  C_ACCTBAL     DECIMAL(15,2),
+  C_MKTSEGMENT  STRING,
+  C_COMMENT     STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/customer.csv'
-INTO TABLE dbt.customer;
+INSERT INTO db.customer
+SELECT CAST(_c0 AS INT), _c1, _c2, CAST(_c3 AS INT), _c4, CAST(_c5 AS DECIMAL(15,2)), _c6, _c7
+FROM csv.`/mnt/project/seeds/customer.csv`;
 
-CREATE TABLE db.orders  ( O_ORDERKEY       INTEGER NOT NULL,
-                           O_CUSTKEY        INTEGER NOT NULL,
-                           O_ORDERSTATUS    CHAR(1) NOT NULL,
-                           O_TOTALPRICE     DECIMAL(15,2) NOT NULL,
-                           O_ORDERDATE      DATE NOT NULL,
-                           O_ORDERPRIORITY  CHAR(15) NOT NULL,  
-                           O_CLERK          CHAR(15) NOT NULL, 
-                           O_SHIPPRIORITY   INTEGER NOT NULL,
-                           O_COMMENT        VARCHAR(79) NOT NULL)
-USING iceberg
-LOCATION '/mnt/project/warehouse/orders';
+-- Orders
+CREATE TABLE db.orders (
+  O_ORDERKEY       INTEGER NOT NULL,
+  O_CUSTKEY        INTEGER NOT NULL,
+  O_ORDERSTATUS    STRING NOT NULL,
+  O_TOTALPRICE     DECIMAL(15,2) NOT NULL,
+  O_ORDERDATE      DATE NOT NULL,
+  O_ORDERPRIORITY  STRING NOT NULL,
+  O_CLERK          STRING NOT NULL,
+  O_SHIPPRIORITY   INTEGER NOT NULL,
+  O_COMMENT        STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/orders.csv'
-INTO TABLE db.orders;
+INSERT INTO db.orders
+SELECT CAST(_c0 AS INT), CAST(_c1 AS INT), _c2, CAST(_c3 AS DECIMAL(15,2)), CAST(_c4 AS DATE), _c5, _c6, CAST(_c7 AS INT), _c8
+FROM csv.`/mnt/project/seeds/orders.csv`;
 
-CREATE TABLE db.lineitem ( L_ORDERKEY    INTEGER NOT NULL,
-                             L_PARTKEY     INTEGER NOT NULL,
-                             L_SUPPKEY     INTEGER NOT NULL,
-                             L_LINENUMBER  INTEGER NOT NULL,
-                             L_QUANTITY    DECIMAL(15,2) NOT NULL,
-                             L_EXTENDEDPRICE  DECIMAL(15,2) NOT NULL,
-                             L_DISCOUNT    DECIMAL(15,2) NOT NULL,
-                             L_TAX         DECIMAL(15,2) NOT NULL,
-                             L_RETURNFLAG  CHAR(1) NOT NULL,
-                             L_LINESTATUS  CHAR(1) NOT NULL,
-                             L_SHIPDATE    DATE NOT NULL,
-                             L_COMMITDATE  DATE NOT NULL,
-                             L_RECEIPTDATE DATE NOT NULL,
-                             L_SHIPINSTRUCT CHAR(25) NOT NULL,
-                             L_SHIPMODE     CHAR(10) NOT NULL,
-                             L_COMMENT      VARCHAR(44) NOT NULL)
-USING iceberg
-LOCATION '/mnt/project/warehouse/lineitem';
+-- Lineitem
+CREATE TABLE db.lineitem (
+  L_ORDERKEY      INTEGER NOT NULL,
+  L_PARTKEY       INTEGER NOT NULL,
+  L_SUPPKEY       INTEGER NOT NULL,
+  L_LINENUMBER    INTEGER NOT NULL,
+  L_QUANTITY      DECIMAL(15,2) NOT NULL,
+  L_EXTENDEDPRICE DECIMAL(15,2) NOT NULL,
+  L_DISCOUNT      DECIMAL(15,2) NOT NULL,
+  L_TAX           DECIMAL(15,2) NOT NULL,
+  L_RETURNFLAG    STRING NOT NULL,
+  L_LINESTATUS    STRING NOT NULL,
+  L_SHIPDATE      DATE NOT NULL,
+  L_COMMITDATE    DATE NOT NULL,
+  L_RECEIPTDATE   DATE NOT NULL,
+  L_SHIPINSTRUCT  STRING NOT NULL,
+  L_SHIPMODE      STRING NOT NULL,
+  L_COMMENT       STRING
+)
+USING iceberg;
 
-LOAD DATA INPATH '/mnt/project/dbt/seeds/lineitem.csv'
-INTO TABLE db.lineitem;
+INSERT INTO db.lineitem
+SELECT CAST(_c0 AS INT), CAST(_c1 AS INT), CAST(_c2 AS INT), CAST(_c3 AS INT),
+       CAST(_c4 AS DECIMAL(15,2)), CAST(_c5 AS DECIMAL(15,2)), CAST(_c6 AS DECIMAL(15,2)),
+       CAST(_c7 AS DECIMAL(15,2)), _c8, _c9,
+       CAST(_c10 AS DATE), CAST(_c11 AS DATE), CAST(_c12 AS DATE),
+       _c13, _c14, _c15
+FROM csv.`/mnt/project/seeds/lineitem.csv`;
